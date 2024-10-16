0a2a8793ce3039d61493d9bf08e5b960ee09f021
cingihimut <cingihimutbanget@gmail.com>
2024-10-16 18:52:58 +0700

    switch to branch master

--- ignition/modules/MockToken.js
+++ ignition/modules/MockToken.js
@@ -2,6 +2,7 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 const { ethers } = require("hardhat");
 
 module.exports = buildModule("MockToken", (m) => {
-  const mockToken = m.contract("MockToken");
+  const initialOwner = "0x0caDDE63e1A3F92d6E754eFb74288810DABFC150"
+  const mockToken = m.contract("MockToken", initialOwner);
   return { mockToken };
 });
--- ignition/modules/SturanNetwork.js
+++ ignition/modules/SturanNetwork.js
@@ -2,8 +2,8 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 const { ethers } = require("hardhat");
 
 module.exports = buildModule("SturanNetwork", (m) => {
-  const mockUSDC = "0x57c58d1869e9c354683C2477759402ba7Cb99043";
-  const initialOwner = "0x0caDDE63e1A3F92d6E754eFb74288810DABFC150";
-  const sturanNetwork = m.contract("SturanNetwork", [mockUSDC, initialOwner]);
+  const mockToken = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+  const initialOwner = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"; //localhost account
+  const sturanNetwork = m.contract("SturanNetwork", [mockToken, initialOwner]);
   return { sturanNetwork };
 });
